3393139de1830827b14c6c450082d0901b36879a
Kemal Mustafic <kemalmustafic1@gmail.com>
2018-10-13 19:36:56 +0200

    Fixed build for native module

---
+++ constants/Constants.js
@@ -0,0 +1,17 @@
+const APPLICATION_NAME = 'test';
+const APPLICATION_ID = 'test' 
+const CONTRACT_ID = 'test' 
+const P12_FILENAME = 'test' 
+const P12_EXTENSION = 'test' 
+const P12_PASSPHRASE = 'test' 
+
+const Constants = {
+  APPLICATION_NAME,
+  APPLICATION_ID,
+  CONTRACT_ID,
+  P12_FILENAME,
+  P12_EXTENSION,
+  P12_PASSPHRASE,
+}
+
+module.exports = Object.assign({}, Constants, { Constants })
---
+++ ios/Constants.h
@@ -0,0 +1,9 @@
+//******************************************************
+// digi.me React Native Demo
+// This file has been generated from javascript source
+//******************************************************
+#define APPLICATION_NAME @"test"
+#define APPLICATION_ID @"test"
+#define CONTRACT_ID @"test"
+#define P12_FILENAME @"test"
+#define P12_PASSPHRASE @"test"
