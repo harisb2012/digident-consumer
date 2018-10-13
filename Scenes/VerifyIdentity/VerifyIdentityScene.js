33924c1d9c01e129ad327170e9e2109c134b4b61
Kemal Mustafic <kemalmustafic1@gmail.com>
2018-10-13 15:54:28 +0200

    Add buttons placeholders in verify identity scene

--- Scenes/VerifyIdentity/VerifyIdentityScene.js
+++ Scenes/VerifyIdentity/VerifyIdentityScene.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center'
   }
@@ -13,7 +14,13 @@ export class VerifyIdentityScene extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Verify identity</Text>
+        <TouchableOpacity>
+          <Text> Verify manually </Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+          <Text> Use digi.me </Text>
+        </TouchableOpacity>
       </View>
     )
   }
